6a6c64526795869e113f34ed1c05d620a85a08f7
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-08-12 19:30:52 +0000

    commit

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', function(e){
+    setTimeout(()=>{
+        splash.classList.add('display-none');
+    },4000);
+})
+
+
+
+
+
+
+
+
+
+
+
